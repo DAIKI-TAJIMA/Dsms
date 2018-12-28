8356709dc00923d1bdaf88f0abd6e5523d3d2799
田島 大輝 <ditajima@APDEV3.local>
2018-12-28 10:56:37 +0900

    プロジェクト ファイルを追加します。

---
+++ Dsms/Areas/DME001/Controllers/DME001_001Controller.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dsms.Areas.DME001.Controllers
+{
+    public class DME001_001Controller : Controller
+    {
+        public IActionResult Init()
+        {
+            return View();
+        }
+    }
+}
---
+++ Dsms/Areas/DME001/ViewModels/DME001_001ViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dsms.Areas.DME001.ViewModels
+{
+    public class DME001_001ViewModel
+    {
+    }
+}
