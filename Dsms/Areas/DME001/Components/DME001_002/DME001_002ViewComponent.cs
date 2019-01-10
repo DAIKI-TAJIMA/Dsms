33732b32a195d81527b5374affa987c8fdb8da36
ditajima <di-tajima@futureinn.co.jp>
2019-01-10 13:26:12 +0900

    コミット

---
+++ Dsms/Areas/DME001/Components/DME001_002/DME001_002ViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Dsms.ViewModels;
+using System.Threading.Tasks;
+
+namespace Dsms.Components
+{
+    [ViewComponent(Name = "DME001_002")]
+    public class DME001_002ViewComponent : ViewComponent
+    {
+        public DME001_002ViewComponent()
+        {
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            // ビューモデルを生成する。
+            DME001_002ViewModel viewModel = new DME001_002ViewModel();
+            return await Task.FromResult(View(viewModel));
+        }
+    }
+}
---
+++ Dsms/Areas/DME001/ViewModels/DME001_002ViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Dsms.ViewModels
+{
+    [Serializable]
+    public class DME001_002ViewModel
+    {
+        /// <summary>
+        /// ユーザコード
+        /// </summary>
+        [Required]
+        [Display(Name = "ユーザコード")]
+        public string UserCd { get; set; }
+    }
+}
